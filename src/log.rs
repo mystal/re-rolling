0cf255ef2532abe6356b79d2502d561f4cfdb72e
Gabriel Martinez <reitaka@gmail.com>
2023-11-30 21:38:55 -0800

    Fix setting logging verbosity due to updated crate name

--- src/log.rs
+++ src/log.rs
@@ -4,10 +4,10 @@ pub fn log_plugin() -> LogPlugin {
     // Configure logging.
     let mut plugin = LogPlugin::default();
     if cfg!(feature = "verbose_logs") {
-        plugin.filter.push_str(",info,gmtk_2022=trace");
+        plugin.filter.push_str(",info,re_rolling=trace");
         plugin.level = Level::TRACE;
     } else if cfg!(debug_assertions) {
-        plugin.filter.push_str(",info,gmtk_2022=debug");
+        plugin.filter.push_str(",info,re_rolling=debug");
         plugin.level = Level::DEBUG;
     }
     plugin
